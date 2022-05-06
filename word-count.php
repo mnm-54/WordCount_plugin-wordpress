e4a6eea079cfbc60c2753c724bc94f2e42b8842d
mnm-54 <shahriarmun54@gmail.com>
2022-05-06 12:37:15 +0600

    plugin definition

---
+++ word-count.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Word Count
+Plugin URI: 
+Description: Count Words from any WordPress Post
+Version: 1.0
+Author: munem
+Author URI: https://github.com/mnm-54
+License: GPLv2 or later
+Text Domain: word-count
+Domain Path: /languages/
+*/
